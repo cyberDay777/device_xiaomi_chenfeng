d5ef9251ed4d1626b76c9cd6caf5631d77b2c662
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-18 14:55:24 +0530

    Chenfeng: Import overlays
    Change-Id: I057af9c088eb29966c8430e424a77c2881bc4e16

--- device.mk
+++ device.mk
@@ -63,6 +63,16 @@ PRODUCT_PACKAGES += \
  PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Overlays
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayChenfeng \
+    FrameworkOverlayChenfeng \
+    SecureElementOverlayChenfeng \
+    SettingsOverlayChenfeng \
+    SystemUIOverlayChenfeng \
+    TelephonyOverlayChenfeng \
+    WifiOverlayChenfeng
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := gz
