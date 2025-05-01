bb3ecab62d5e1fa0be6f8b1c4daae00744dadf6b
cyberDay777 <asacademybhind@gmail.com>
2025-05-01 14:55:08 +0530

    chenfeng: Setup Kernel args
    Change-Id: Ib27538486033ba7cc7e80db0350a242135015817

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-#
+ #
  # Copyright (C) 2024 The LineageOS Project
  #
  # SPDX-License-Identifier: Apache-2.0
@@ -19,6 +19,42 @@
  
  # Hardware
  BOARD_USES_QCOM_HARDWARE := true
+
+# Kernel
+ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+ BOARD_RAMDISK_USE_LZ4 := true
+ TARGET_NEEDS_DTBOIMAGE := true
  
+ BOARD_KERNEL_BASE := 0x00000000
+ BOARD_KERNEL_PAGESIZE := 4096
+ BOARD_KERNEL_IMAGE_NAME := Image
+ 
+ TARGET_KERNEL_SOURCE := kernel/xiaomi/chenfeng
+ TARGET_KERNEL_CONFIG := \
+     gki_defconfig
+ 
+ BOARD_BOOT_HEADER_VERSION := 4
+ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+ 
+ BOARD_INIT_BOOT_HEADER_VERSION := 4
+ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+ 
+ BOARD_KERNEL_CMDLINE := \
+     video=vfb:640x400,bpp=32,memsize=3072000 \
+     swinfo.fingerprint=chenfeng:14/OS2.0.2.0.VNJINXM:user \ 
+     mtdoops.fingerprint=chenfeng:14/OS2.0.2.0.VNJINXM:user bootconfig \
+
+ BOARD_BOOTCONFIG := \
+     androidboot.hardware=qcom \
+     androidboot.memcg=1 \
+     androidboot.usbcontroller=a600000.dwc3 \
+     androidboot.load_modules_parallel=true \
+     androidboot.vendor.qspa=true \
+     androidboot.hypervisor.protected_vm.supported=false \
+     androidboot.selinux=permissive
+  
  # Platform
  TARGET_BOARD_PLATFORM := pineapple
+
+# Recovery
+ BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
